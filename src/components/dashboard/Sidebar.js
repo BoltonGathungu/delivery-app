7c831dd4c826fa84411967a3ef3a6a698ce26911
stephenkite <kite1533@gmail.com>
2024-07-30 07:04:20 +0300

    changes that din't work

--- src/components/dashboard/Sidebar.js
+++ src/components/dashboard/Sidebar.js
@@ -4,11 +4,16 @@ import { FaCartPlus } from "react-icons/fa";
 import { FaUpload } from "react-icons/fa";
 import { IoFastFood } from "react-icons/io5";
 import { TbCategoryPlus } from "react-icons/tb";
-import { Link } from "react-router-dom";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import { useStore } from "../../store";
+import { useState } from "react";
 
 function Sidebar() {
   const products = useStore((state) => state.products);
+  const [resolvedPath, setResolvedPath] = useState();
+
+  const isActive = useMatch({ path: useResolvedPath(resolvedPath).pathname, end: true})
+
 
   return (
     <div className="w-[250px] h-screen overflow-y-scroll bg-black text-white pt-5">
@@ -19,8 +24,8 @@ function Sidebar() {
         <div className="">Delivery App</div>
       </div>
 
-      <Link to="/dashboard/products">
-        <div className="flex gap-x-5 items-center px-4 py-2 mt-2 cursor-pointer hover:bg-blue-500 mx-1 rounded-full">
+      <Link to="/dashboard/products" onClick={()=>setResolvedPath("/dashboard/products")}>
+        <div className={isActive ? "flex gap-x-5 items-center px-4 py-2 mt-2 cursor-pointer bg-blue-500 mx-1 rounded-full" : "flex gap-x-5 items-center px-4 py-2 mt-2 cursor-pointer hover:bg-blue-500 mx-1 rounded-full"}>
           <div className="p-2 ">
             <AiFillDashboard className="text-xl" />
           </div>
@@ -30,7 +35,7 @@ function Sidebar() {
         </div>
       </Link>
 
-      <Link to="/dashboard/sales">
+      <Link to="/dashboard/sales" onClick={()=>setResolvedPath("/dashboard/sales")}>
         <div className="flex gap-x-5 items-center px-4 py-2 mt-2 cursor-pointer hover:bg-blue-500 mx-1 rounded-full">
           <div className="p-2 ">
             <FaCartPlus className="" />
@@ -40,7 +45,7 @@ function Sidebar() {
       </Link>
 
       <Link
-        to="/dashboard/upload"
+        to="/dashboard/upload" onClick={()=>setResolvedPath("/dashboard/upload")}
         className="flex gap-x-5 items-center px-4 py-2 mt-2 cursor-pointer hover:bg-blue-500 mx-1 rounded-full"
       >
         <div className="p-2 ">
@@ -49,7 +54,7 @@ function Sidebar() {
         <div className="">Upload</div>
       </Link>
       <Link
-        to="/dashboard/addon"
+        to="/dashboard/addon" onClick={()=>setResolvedPath("/dashboard/addon")}
         className="flex gap-x-5 items-center px-4 py-2 mt-2 cursor-pointer hover:bg-blue-500 mx-1 rounded-full"
       >
         <div className="p-2 ">
@@ -58,7 +63,7 @@ function Sidebar() {
         <div className="">Addon</div>
       </Link>
       <Link
-        to="/dashboard/categories"
+        to="/dashboard/categories" onClick={()=>setResolvedPath("/dashboard/categories")}
         className="flex gap-x-5 items-center px-4 py-2 mt-2 cursor-pointer hover:bg-blue-500 mx-1 rounded-full"
       >
         <div className="p-2 ">
